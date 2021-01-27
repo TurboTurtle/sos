1a50713f8362ca42ebee8c0f0f83a0b90ca8764a
Jake Hunsaker <jhunsake@redhat.com>
2021-01-27 09:55:39 -0500

    [wip][tests] Start using avocado for test suite
    This commit represents the start of an overhaul of the test suite used
    by sos. Note that several more commits to follow will be required in
    order for the test suite to be considered stable.
    
    The new test suite will use the avocado-framework to build out new
    tests.
    
    This first part first moves the existing unittests into a new 'stage0'
    subdir, which follows a new naming scheme where:
    
    	stage0 -> Unittests
    	stage1 -> Basic function tests, no mocking allowed
    	stage2 -> Mocked tests for specific scenarios/regressions
    	stage3 -> Complex setups for layed products/environments
    
    At the moment, these unittests are not updated for avocado, though most
    should still work with `nosetest` directly.
    
    A new set of base classes is defined in tests/sos_tests.py which provide
    the foundation for actual tests cases. This approach entails new test
    cases subclassing a base class, such as the new `StageOneReportTest`,
    and setting the `sos_cmd` class attr to the _options_ for an sos report
    run. By default `sos report --batch` will be run, and targeted to the
    test job's directory as a tmpdir.
    
    Each sos command will be executed once, and all test_* methods within a
    test case that subclasses `StageOneReportTest` will be checked against
    the output of that execution. Note that this diverges from avocado's
    typical approach where each test_* method is performed against a brand
    new instance of the class (thus meaning any setup including our sos
    report run would normally be run fresh). However, after speaking with
    the avocado devel team, this is still seen as a valid pattern for the
    framework.
    
    Future commits may move the basic test cases into new subdirs, such as
    `tests/stage1`, however that will require providing a testing entry
    point if the project intends to still allow running the test suite from
    the git checkout without doing a local install of the repo.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ tests/report_with_mask.py
@@ -0,0 +1,66 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos_tests import StageOneReportTest
+
+import re
+
+
+class ReportWithMask(StageOneReportTest):
+    """Testing around basic --clean/--mask usage and expectations
+
+    :avocado: tags=stageone
+    """
+
+    sos_cmd = '--mask -o host,networking'
+
+    def test_mask_was_run(self):
+        self.assertOutputContains('Beginning obfuscation')
+        self.assertOutputContains('Obfuscation completed')
+
+    def test_private_map_was_generated(self):
+        self.assertOutputContains('A mapping of obfuscated elements is available at')
+        map_file = re.findall('/.*sosreport-.*-private_map', self.cmd_output.stdout)[-1]
+        self.assertFileExists(map_file)
+
+    def test_tarball_named_obfuscated(self):
+        self.assertTrue('obfuscated' in self.archive)
+
+    def test_localhost_was_obfuscated(self):
+        self.assertFileHasContent('/etc/hostname', 'host0')
+
+    def test_ip_address_was_obfuscated(self):
+        self.assertFileHasContent('ip_addr', '100.0.0.1')
+
+    def test_loopback_was_not_obfuscated(self):
+        self.assertFileHasContent('ip_addr', '127.0.0.1/8')
+
+    def test_mac_addrs_were_obfuscated(self):
+        content = self.get_file_content('sos_commands/networking/ip_maddr_show')
+        for line in content.splitlines():
+            if line.strip().startswith('link'):
+                mac = line.strip().split()[1]
+                assert mac.startswith('53:4f:53'), "Found unobfuscated mac addr %s" % mac
+
+
+class ReportWithCleanedKeywords(StageOneReportTest):
+    """Testing for obfuscated keywords provided by the user
+
+    :avocado: tags=stageone
+    """
+
+    sos_cmd = '--clean -o filesys,kernel --keywords=fstab,Linux'
+
+    # Ok, sort of cheesy here but this does actually test filename changes on
+    # a file common to all distros
+    def test_filename_obfuscated(self):
+        self.assertFileNotCollected('/etc/fstab')
+        self.assertFileGlobInArchive('/etc/obfuscatedword*')
+
+    def test_keyword_obfuscated_in_file(self):
+        self.assertFileNotHasContent('sos_commands/kernel/uname_-a', 'Linux')
